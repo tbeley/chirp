6f948345309cd86b3ea71a3bae42202271f45018
Thibaud <thibaud.beley@gmail.com>
2023-06-27 16:59:29 +0200

    font sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,9 +45,9 @@ const PostView = (props: PostWithUser) => {
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
           <span>{`@${author.username}`}</span>
-          <span>{` · ${dayjs(post.createdAt).fromNow()}`}</span>
+          <span className="font-thin">{` · ${dayjs(post.createdAt).fromNow()}`}</span>
         </div>
-        <span className="">{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
