442e2da9067c37c886105044bec18f0b9bcc10c8
Thibaud <thibaud.beley@gmail.com>
2023-06-27 13:49:13 +0200

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
+import { api } from "~/utils/api";
 
 export default function Home() {
   const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -15,6 +17,11 @@ export default function Home() {
         <div>
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((post) => (
+            <div key="post.id">{post.content}</div>
+          ))}
         </div>
       </main>
     </>
